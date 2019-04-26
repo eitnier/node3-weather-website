89e812ef8170df0711443799f658b57e9427beba
eitnier <eitnier@gmail.com>
2019-04-26 13:51:54 -0700

    Added UV to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback("Unable to find location.", undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees outside. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            console.log(body.currently)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees outside. There is a ' + body.currently.precipProbability + '% chance of rain. The UV index is ' + body.currently.uvIndex);
         }
     })
 }
